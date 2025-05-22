fab1425687df52ae678dd20fd8251a5202aa201b
mascai <msumoskaleno@gmail.com>
2025-05-22 14:48:05 +0300

    Remove autoupgrade

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List, Dict, Any
-import asyncio
 from alembic.config import Config
-from alembic import command
+from database.session import engine
 
 from models.users import User
 from schemas.users import UserSchema, BaseUserSchema
@@ -13,15 +12,6 @@ from api.v1.users import users_router
 application = FastAPI()
 application.include_router(users_router)
 
-@application.on_event("startup")
-async def startup_event():
-    # Run alembic migrations
-    alembic_cfg = Config("alembic.ini")
-    try:
-        command.upgrade(alembic_cfg, "head")
-    except Exception as e:
-        print(f"Error running migrations: {e}")
-        raise e
 
 @application.get("/health_check")
 async def health_check():
